bd78862554fe3c00a04505c67ff768b18a873361
behn4m <behnam.shakibafar@gmail.com>
2023-08-22 22:49:13 -0400

    add lvgl - create rm67162 display driver - update configuration

--- main/main.c
+++ main/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "lvgl.h"
+
+#include "rm67162.h"
+
+void lv_example_get_started_1(void)
+{
+
+}
 
 void app_main(void)
 {
+    rm67162_init();
+    lv_init();
 
+    lcd_fill(100, 100, 536, 240, 0x1221);
 }
