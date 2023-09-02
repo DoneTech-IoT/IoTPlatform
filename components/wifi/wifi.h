9b0db1103de529fee5b7d48afb4da20d3d69f3db
behn4m <behnam.shakibafar@gmail.com>
2023-09-02 10:02:21 -0400

    WiFi component added to handlle wifi connection.

---
+++ components/wifi/wifi.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "stdint.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+void connect_wifi();
--- main/main.c
+++ main/main.c
@@ -5,19 +5,19 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+
 #include "lvgl.h"
-
 #include "rm67162.h"
+#include "wifi.h"
 
-void lv_example_get_started_1(void)
-{
-
-}
 
 void app_main(void)
 {
     rm67162_init();
     lv_init();
+
+    nvs_flash_init();
+    connect_wifi();
 
     lcd_fill(100, 100, 536, 240, 0x1221);
 }
