26b50229c68287f6e1502a843d9a0e0794ce0575
ehsanziyaee <ehsan.ziyaee@gmail.com>
2024-01-29 14:58:29 +0330

    remove GlobalConfig and use SpiffsExistenceCheck insteed it directly (with external flag)

--- components/nvsFlash/SpiffsManger.c
+++ components/nvsFlash/SpiffsManger.c
@@ -10,7 +10,6 @@
 static const char *TAG = "Spiffs";
 
 static SemaphoreHandle_t SpiffsMutex;   
-static GlobalInitInterfaceHandler_t *InterfaceHandler;
 
 /**
  * @brief Perform a SPIFFS check on the specified partition.
@@ -289,83 +288,6 @@ bool SpiffsRemoveFile(char *addressInSpiffs)
 }
 
 /**
- * @brief This function searches for a specific value in a JSON string based on a given key.
- * @param[in] jsonString The JSON string to search within.
- * @param[in] key The key to search for.
- * @param[out] valueBuffer The buffer to store the found value.
- * @param[in] bufferSize The size of the value buffer.
- * @return Returns true if the value is found and copied to the value buffer, and false otherwise.
- */
-static char *FindValueByKey(const char *jsonStr, const char *key, char *ValueBuffer, size_t SizeOfValueBuf)
-{
-    cJSON *root = cJSON_Parse(jsonStr);
-    if (root == NULL)
-    {
-        ESP_LOGE(TAG,"Failed to parse JSON.\n");
-        return NULL;
-    }
-    cJSON *item = cJSON_GetObjectItemCaseSensitive(root, key);
-    if (item == NULL)
-    {
-        ESP_LOGE(TAG,"Key '%s' not found.\n", key);
-        cJSON_Delete(root);
-        return NULL;
-    }
-    //  value = item->valuestring;
-    strncpy(ValueBuffer, item->valuestring, sizeof(ValueBuffer) - 1);
-    ValueBuffer[sizeof(ValueBuffer) - 1] = '\0';
-    cJSON_Delete(root);
-    return ValueBuffer;
-}
-
-/**
- * @brief This function parses a JSON string and prints the key-value pairs.
- * @param[in] jsonString The JSON string to parse and print.
- */
-static void ParseOutputJSON(const char *jsonStr)
-{
-    // CountOfKeyValues(jsonStr);
-    char buf[100];
-    FindValueByKey(jsonStr, "Key1", buf, sizeof(buf));
-    ESP_LOGI(TAG, "\n%s\n", buf);
-    cJSON *root = cJSON_Parse(jsonStr);
-    if (root == NULL)
-    {
-        ESP_LOGE(TAG,"Failed to parse JSON.\n");
-        return;
-    }
-    cJSON *item = root->child;
-    while (item != NULL)
-    {
-        if (cJSON_IsString(item))
-        {
-            ESP_LOGI(TAG, "%s: %s\n", item->string, item->valuestring);
-        }
-        item = item->next;
-    }
-    cJSON_Delete(root);
-}
-
-/**
- * @brief This function counts the number of key-value pairs in a JSON string.
- * @param[in] jsonString The JSON string to count the key-value pairs.
- * @return The number of key-value pairs.
- */
-static int CountOfKeyValues(const char *jsonStr)
-{
-    cJSON *root = cJSON_Parse(jsonStr);
-    if (root == NULL)
-    {
-        ESP_LOGE(TAG,"Failed to parse JSON.\n");
-        return -1;
-    }
-    int count = cJSON_GetArraySize(root);
-    ESP_LOGI(TAG, "count of keys = %d\n", count);
-    cJSON_Delete(root);
-    return count;
-}
-
-/**
  * @brief This function saves key-value pairs in a file in SPIFFS with a text format.
  * @param[in] filename The name of the file to save the key-value pairs.
  * @param[in] key key-var
@@ -428,27 +350,6 @@ void SpiffsReadTxtFile(char *addressInSpiffs, char *key, char *value, ...)
     free(InternalBuf);
     va_end(args);
     cJSON_Delete(root);
-}
-
-/**
- * @brief This function does global initialization for Spiffs, checks for save existence, and sends a signal if it exists
- * @param[in] GlobalInitInterfaceHandler pointer of GlobalInitHandeler for its content access
- */
-void SpiffsGlobalConfig(GlobalInitInterfaceHandler_t *GlobalInitInterfaceHandler)
-{
-    InterfaceHandler = GlobalInitInterfaceHandler;
-    SpiffsInit();
-    if (SpiffsExistenceCheck(WifiConfigDirectoryAddressInSpiffs) == 1)
-    {
-        xSemaphoreGive(InterfaceHandler->WifiParamExistenceCheckerSemaphore);
-    }
-
-    #ifdef SpotifyEnable
-    if (SpiffsExistenceCheck(SpotifyConfigAddressInSpiffs) == 1)
-    {
-        xSemaphoreGive(InterfaceHandler->WorkWithStorageInSpotifyComponentSemaphore);
-    }
-    #endif
 }
 
 /**
