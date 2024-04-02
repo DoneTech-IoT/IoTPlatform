c5a23c53f978ecdb01df1ed9e4cb1c7daf27f525
MohammadAbbasi76 <work.abbasii@gmail.com>
2024-04-02 16:53:52 +0330

    all task gettting work with together

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -77,14 +77,13 @@ extern "C" void app_main()
     GUI_TaskInit();
     freeHeapSize = xPortGetFreeHeapSize();
     ESP_LOGW("TAG", "Free Heap Size: %u bytes\n", freeHeapSize);
-    // EXT_RAM_ATTR
     GlobalInit();
     nvsFlashInit();
     SpiffsGlobalConfig();
-    // BottomCallBackFunctions_t BottomCallBackFunctions;
-    // BottomCallBackFunctions.BackBottomCallBack = BackBottomCallBack_;
-    // BottomCallBackFunctions.AcceptBottomCallBack = AcceptBottomCallBack_;
-    // GPIO_init(BottomCallBackFunctions);
+    BottomCallBackFunctions_t BottomCallBackFunctions;
+    BottomCallBackFunctions.BackBottomCallBack = BackBottomCallBack_;
+    BottomCallBackFunctions.AcceptBottomCallBack = AcceptBottomCallBack_;
+    GPIO_init(BottomCallBackFunctions);
 
     MatterInterfaceHandler.SharedBufQueue = &MatterBufQueue;
     MatterInterfaceHandler.SharedSemaphore = &MatterSemaphore;
@@ -92,35 +91,35 @@ extern "C" void app_main()
     Matter_TaskInit(&MatterInterfaceHandler);
     vTaskDelay((pdMS_TO_TICKS(SEC * 5)));
 
-    // SpotifyInterfaceHandler.IsSpotifyAuthorizedSemaphore = &IsSpotifyAuthorizedSemaphore;
-    // SpotifyInterfaceHandler.ConfigAddressInSpiffs = SpotifyConfigAddressInSpiffs;
-    // Spotify_TaskInit(&SpotifyInterfaceHandler);
-    // unsigned int numberOfTasks = uxTaskGetNumberOfTasks();
-    // printf("Number of tasks: %u\n", numberOfTasks);
-    // printf("CONFIG_FREERTOS_HZ =%d\n", CONFIG_FREERTOS_HZ);
-    // freeHeapSize = xPortGetFreeHeapSize();
-    // ESP_LOGE(TAG, "Free Heap Size: %u bytes\n", freeHeapSize);
-    // // after this semaphore you can use playback command function in every where !
-    // if (xSemaphoreTake(IsSpotifyAuthorizedSemaphore, portMAX_DELAY) == pdTRUE)
-    // {
-    //     bool CommandResult = false;
-    //     CommandResult = Spotify_SendCommand(SpotifyInterfaceHandler, GetUserInfo);
-    //     if (CommandResult == false)
-    //     {
-    //         ESP_LOGE(TAG, "User info update failed");
-    //         return;
-    //     }
-    //     ESP_LOGI(TAG, "User info updated");
-    //     TimerHandle_t xTimer = xTimerCreate("update", TIMER_TIME, pdTRUE, NULL, SpotifyPeriodicTimer);
-    //     xTimerStart(xTimer, 0);
-    //     if (xTimer != NULL)
-    //     {
-    //         if (xTimerStart(xTimer, 0) == pdPASS)
-    //         {
-    //             ESP_LOGI(TAG, "Timer getting start");
-    //         }
-    //     }
-    // }
+    SpotifyInterfaceHandler.IsSpotifyAuthorizedSemaphore = &IsSpotifyAuthorizedSemaphore;
+    SpotifyInterfaceHandler.ConfigAddressInSpiffs = SpotifyConfigAddressInSpiffs;
+    Spotify_TaskInit(&SpotifyInterfaceHandler);
+    unsigned int numberOfTasks = uxTaskGetNumberOfTasks();
+    printf("Number of tasks: %u\n", numberOfTasks);
+    printf("CONFIG_FREERTOS_HZ =%d\n", CONFIG_FREERTOS_HZ);
+    freeHeapSize = xPortGetFreeHeapSize();
+    ESP_LOGE(TAG, "Free Heap Size: %u bytes\n", freeHeapSize);
+    // after this semaphore you can use playback command function in every where !
+    if (xSemaphoreTake(IsSpotifyAuthorizedSemaphore, portMAX_DELAY) == pdTRUE)
+    {
+        bool CommandResult = false;
+        CommandResult = Spotify_SendCommand(SpotifyInterfaceHandler, GetUserInfo);
+        if (CommandResult == false)
+        {
+            ESP_LOGE(TAG, "User info update failed");
+            return;
+        }
+        ESP_LOGI(TAG, "User info updated");
+        TimerHandle_t xTimer = xTimerCreate("update", TIMER_TIME, pdTRUE, NULL, SpotifyPeriodicTimer);
+        xTimerStart(xTimer, 0);
+        if (xTimer != NULL)
+        {
+            if (xTimerStart(xTimer, 0) == pdPASS)
+            {
+                ESP_LOGI(TAG, "Timer getting start");
+            }
+        }
+    }
 }
 
 void MatterAttributeUpdateCBMain(
