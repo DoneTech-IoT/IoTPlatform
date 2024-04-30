7927633a3744ad7651c45d34212ddbf45ac966aa
MohammadAbbasi76 <work.abbasii@gmail.com>
2024-04-30 17:21:48 +0330

    bomp

--- main/ServiceManger.h
+++ main/ServiceManger.h
@@ -20,25 +20,25 @@ TaskHandle_t xTaskGUIHandler;
 #include <stdint.h>
 
 // Define enum for RAM type
-// typedef enum {
-//     PSRAM,
-//     SRAM
-// } RAM_Type;
+typedef enum {
+    PSRAM,
+    SRAM
+} RAM_Type;
 
-// // Define struct for Task
-// typedef struct {
-//     char name[32];        // Task name
-//     uint8_t priority;     // Priority of the task
-//     uint32_t startupRAM;  // Amount of RAM needed at startup time
-//     void (*taskHandler)(void*); // Pointer to the task handler function
-//     RAM_Type ramType;     // RAM type where task occupies (PSRAM or SRAM)
-// } Task;
+// Define struct for Task
+typedef struct {
+    char name[32];        // Task name
+    uint8_t priority;     // Priority of the task
+    uint32_t startupRAM;  // Amount of RAM needed at startup time
+    void (*taskHandler)(void*); // Pointer to the task handler function
+    RAM_Type ramType;     // RAM type where task occupies (PSRAM or SRAM)
+} Task;
 
-// // Define struct for Task Manager
-// typedef struct {
-//     Task tasks[10];       // Array of tasks (assuming a maximum of 10 tasks)
-//     uint32_t maxSRAM;     // Maximum SRAM needed by any task
-// } ServiceManger;
-// void testServiceManger();
+// Define struct for Task Manager
+typedef struct {
+    Task tasks[10];       // Array of tasks (assuming a maximum of 10 tasks)
+    uint32_t maxSRAM;     // Maximum SRAM needed by any task
+} ServiceManger;
+void testServiceManger();
 
 #endif
