79ec8088d657aa19725e177559fa75a7068ce1e2
ehsanziyaee <ehsan.ziyaee@gmail.com>
2024-07-20 18:03:43 +0330

    impelement of done_MasterPower_key

--- components/MatterInterface/DoneMatterEndpoint.cpp
+++ components/MatterInterface/DoneMatterEndpoint.cpp
@@ -9,6 +9,47 @@ namespace esp_matter {
 using namespace cluster;
 namespace endpoint {
 
+namespace done_MasterPower_key {
+uint32_t get_device_type_id()
+{
+    return ESP_MATTER_DONE_MASTER_POWER_KEY_DEVICE_TYPE_ID;
+}
+
+uint8_t get_device_type_version()
+{
+    return ESP_MATTER_DONE_MASTER_POWER_KEY_DEVICE_TYPE_VERSION;
+}
+
+endpoint_t *create(node_t *node, config_t *config, uint8_t flags, void *priv_data)
+{
+    endpoint_t *endpoint = endpoint::create(node, flags, priv_data);
+    add(endpoint, config);
+    return endpoint;
+}
+
+esp_err_t add(endpoint_t *endpoint, config_t *config)
+{
+    if (!endpoint) {
+        ESP_LOGE(TAG, "Endpoint cannot be NULL");
+        return ESP_ERR_INVALID_ARG;
+    }
+    esp_err_t err = add_device_type(endpoint, get_device_type_id(), get_device_type_version());
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to add device type id:%" PRIu32 ",err: %d", get_device_type_id(), err);
+        return err;
+    }
+
+    descriptor::create(endpoint, &(config->descriptor), CLUSTER_FLAG_SERVER);
+    cluster_t *identify_cluster = identify::create(endpoint, &(config->identify), CLUSTER_FLAG_SERVER);
+    identify::command::create_trigger_effect(identify_cluster);    
+    switch_cluster::create(endpoint, &(config->switch_cluster), CLUSTER_FLAG_SERVER);
+    operational_state::create(endpoint, &(config->operational_state), CLUSTER_FLAG_SERVER);
+    on_off::create(endpoint, &(config->on_off), CLUSTER_FLAG_SERVER,            on_off::feature::lighting::get_id());
+    
+    return ESP_OK;
+}    
+}//done_MasterPower_key
+
 namespace done_multiFunction_switch {
 uint32_t get_device_type_id()
 {
