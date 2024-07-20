2a9bec80fd20682cbdc91804a8d0aa6bd1caa5c7
ehsanziyaee <ehsan.ziyaee@gmail.com>
2024-07-20 12:16:06 +0330

    impelement some callback body

--- components/MatterInterface/DoneCoffeeMaker.h
+++ components/MatterInterface/DoneCoffeeMaker.h
@@ -7,6 +7,19 @@
 #define ESP_MATTER_DONE_COFFEE_MAKER_DEVICE_TYPE_ID 0x1001
 #define ESP_MATTER_DONE_COFFEE_MAKER_DEVICE_TYPE_VERSION 1
 
+typedef enum {
+    INCREMENT_MODE = 0,
+    DECREMENT_MODE = 1,
+    EXPLICIT_MODE = 2    
+}LevelControlCurrentValueMode_t;
+
+typedef enum {
+    DONT_CARE = 0,
+    GRINDER_MODE = 1,
+    COFFEE_MODE = 2,
+    TEA_MODE = 3    
+}CookingMode_t;
+
 esp_err_t create_DoneCoffeeMaker(esp_matter::node_t* node);
 
 #endif //DONE_COFFEE_MAKER_H
