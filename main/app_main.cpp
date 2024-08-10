3f4cdaa3f41cfb71b195a59d7e91e80eb94501f3
MohammadAbbasi76 <work.abbasii@gmail.com>
2024-08-10 14:07:36 +0330

    save

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -1,25 +1,32 @@
 #include "lvglGui.h"
 #include "GlobalInit.h"
 #include "nvsFlash.h"
+#include "SpotifyInterface.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "Setup_GPIO.h"
+// #include "MatterInterface.h"
 #include "ServiceManger.h"
 #include "Custom_Log.h"
 #define TIMER_TIME pdMS_TO_TICKS(500) // in millis
+
+#ifdef CONFIG_DONE_COMPONENT_MATTER
+#endif
 // ****************************** GLobal Variables ****************************** //
-static const char *TAG = "Main";
+static const char *TAG = "Main";       
+
+
 
 extern "C" void app_main()
 {
 
-    // Log_RamOccupy("main", "service manager");
+    Log_RamOccupy("main", "service manager");
 
     ServiceMangerTaskInit();
     vTaskDelay(pdMS_TO_TICKS(1 * SEC));
 
     GlobalInit();
     nvsFlashInit();
-    SpiffsGlobalConfig();
-    // GUItest();
+    SpiffsGlobalConfig();  
 }
+
