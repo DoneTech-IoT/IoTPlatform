d08338bdd8b77a08c4d75d941a3a4b4a80836606
Behnam <behnam.shakibafar@gmail.com>
2024-08-09 20:49:17 -0400

    main prepared for new development

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -16,19 +16,23 @@ MatterInterfaceHandler_t MatterInterfaceHandler;
 static const char *TAG = "Main";       
 // ****************************** GLobal Functions ****************************** //
 
-
 void MatterAttributeUpdateCBMain(
     callback_type_t type,
     uint16_t endpoint_id, uint32_t cluster_id,
     uint32_t attribute_id, esp_matter_attr_val_t *val,
     void *priv_data)
 {
-    printf("callback_type_t: %u\n", type);
-    printf("endpoint_id: %u\n", endpoint_id);
-    printf("cluster_id: %lu\n", cluster_id);
-    printf("attribute_id: %lu\n", attribute_id);
-    printf("val: %p\n", val);
+    // printf("callback_type_t: %u\n", type);
+    // printf("endpoint_id: %u\n", endpoint_id);
+    // printf("cluster_id: %lu\n", cluster_id);
+    // printf("attribute_id: %lu\n", attribute_id);
+    // printf("val: %p\n", val);
     // printf("priv_data: %pGlobalInitGlobalInitGlobalInit\n", priv_data);
+}
+
+void MatterNetworkConnected()
+{
+    ESP_LOGI(TAG, "Matter Network Connected\n");
 }
 
 /**
@@ -48,7 +52,7 @@ extern "C" void app_main()
     MatterInterfaceHandler.SharedBufQueue = &MatterBufQueue;
     MatterInterfaceHandler.SharedSemaphore = &MatterSemaphore;
     MatterInterfaceHandler.MatterAttributeUpdateCB = MatterAttributeUpdateCBMain;
-    // MatterInterfaceHandler.ConnectToMatterNetwork = MatterNetworkConnected;
+    MatterInterfaceHandler.ConnectToMatterNetwork = MatterNetworkConnected;
     Matter_TaskInit(&MatterInterfaceHandler);
 #endif
 }
