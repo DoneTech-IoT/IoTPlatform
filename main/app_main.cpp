60d45b47917a4c2f76af421f99287bb5b54eb7f5
MohammadAbbasi76 <work.abbasii@gmail.com>
2024-08-14 15:54:50 +0330

    change service manager

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -2,9 +2,9 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "Setup_GPIO.h"
-#include "ServiceManger.h"
+#include "ServiceManager.h"
 #include "Custom_Log.h"
-#include "MatterInterface.h"
+// #include "MatterInterface.h"
 #define TIMER_TIME pdMS_TO_TICKS(500) // in millis
 
 
@@ -15,18 +15,18 @@ MatterInterfaceHandler_t MatterInterfaceHandler;
 static const char *TAG = "Main";       
 // ****************************** GLobal Functions ****************************** //
 
-void MatterAttributeUpdateCBMain(callback_type_t type,
-                                uint16_t endpoint_id, uint32_t cluster_id,
-                                uint32_t attribute_id, esp_matter_attr_val_t *val,
-                                void *priv_data)
-{
-    // printf("callback_type_t: %u\n", type);
-    // printf("endpoint_id: %u\n", endpoint_id);
-    // printf("cluster_id: %lu\n", cluster_id);
-    // printf("attribute_id: %lu\n", attribute_id);
-    // printf("val: %p\n", val);
-    // printf("priv_data: %pGlobalInitGlobalInitGlobalInit\n", priv_data);
-}
+// void MatterAttributeUpdateCBMain(callback_type_t type,
+//                                 uint16_t endpoint_id, uint32_t cluster_id,
+//                                 uint32_t attribute_id, esp_matter_attr_val_t *val,
+//                                 void *priv_data)
+// {
+//     printf("callback_type_t: %u\n", type);
+//     printf("endpoint_id: %u\n", endpoint_id);
+//     printf("cluster_id: %lu\n", cluster_id);
+//     printf("attribute_id: %lu\n", attribute_id);
+//     printf("val: %p\n", val);
+//     printf("priv_data: %pGlobalInitGlobalInitGlobalInit\n", priv_data);
+// }
 
 void MatterNetworkConnected()
 {
@@ -45,11 +45,11 @@ extern "C" void app_main()
     nvsFlashInit();
 
     Log_RamOccupy("main", "service manager");
-    Log_RamOccupy("main", "Matter usage");
-    MatterInterfaceHandler.SharedBufQueue = &MatterBufQueue;
-    MatterInterfaceHandler.SharedSemaphore = &MatterSemaphore;
-    MatterInterfaceHandler.MatterAttributeUpdateCB = MatterAttributeUpdateCBMain;
-    MatterInterfaceHandler.ConnectToMatterNetwork = MatterNetworkConnected;
-    Matter_TaskInit(&MatterInterfaceHandler);
+    // Log_RamOccupy("main", "Matter usage");
+    // MatterInterfaceHandler.SharedBufQueue = &MatterBufQueue;
+    // MatterInterfaceHandler.SharedSemaphore = &MatterSemaphore;
+    // MatterInterfaceHandler.MatterAttributeUpdateCB = MatterAttributeUpdateCBMain;
+    // MatterInterfaceHandler.ConnectToMatterNetwork = MatterNetworkConnected;
+    // Matter_TaskInit(&MatterInterfaceHandler);
     
 }
