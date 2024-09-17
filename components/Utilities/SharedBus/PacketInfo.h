3ca42df68f269657cefebcdaa8bb2e072b40b30a
m4github <m4ghaniofficial@gmail.com>
2024-09-17 17:39:59 +0330

    init packet info files

---
+++ components/Utilities/SharedBus/PacketInfo.h
@@ -0,0 +1,19 @@
+#ifndef COFFEE_MAKER_IFO_H
+#define COFFEE_MAKER_IFO_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "CoffeeMakerInfo.h"
+
+typedef enum {
+
+} PacketIDs;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //COFFEE_MAKER_IFO_H
