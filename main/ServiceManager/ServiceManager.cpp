45b72bb7293d07224b9cc58c028a866914f5dcc5
m4github <m4ghaniofficial@gmail.com>
2024-09-30 11:44:04 +0330

    rm extra

--- main/ServiceManager/ServiceManager.cpp
+++ main/ServiceManager/ServiceManager.cpp
@@ -173,8 +173,6 @@ void ServiceMangerInit()
     MQTT_InterfaceHandler.IsConnectedSemaphore = &MQTTConnectedSemaphore;
     MQTT_InterfaceHandler.BrokerIncomingDataQueue = &MQTTDataFromBrokerQueue;
     MQTT_TaskInit(&MQTT_InterfaceHandler);
-    vTaskDelay(pdMS_TO_TICKS(100));
-    MQTT_Start();
 #endif
 }
 
