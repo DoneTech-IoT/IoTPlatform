9ba44462204269ffb889c6f9787b99fb7d852003
m4github <m4ghaniofficial@gmail.com>
2024-11-04 16:21:59 +0330

    add more data to packet

--- components/Utilities/SharedBus/include/MqttInfo.h
+++ components/Utilities/SharedBus/include/MqttInfo.h
@@ -6,10 +6,13 @@ extern "C"
 {
 #endif
 
+// Define constants for topic length and raw data length
+#define TOPIC_LEN 50
 #define MQTT_RAW_DATA_LEN 2500
 
 typedef struct
 {
+    char Topic[TOPIC_LEN];
     char CoreRawData[MQTT_RAW_DATA_LEN];
 } MqttPacket;
 
