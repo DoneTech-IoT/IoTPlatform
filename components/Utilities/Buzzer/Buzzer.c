59ecc1c90d25d40dd7c871286ede3f42d7fcae0e
MohammadAbbasi76 <work.abbasii@gmail.com>
2024-11-23 14:46:37 +0330

    deactivate buzzer

--- components/Utilities/Buzzer/Buzzer.c
+++ components/Utilities/Buzzer/Buzzer.c
@@ -18,26 +18,26 @@ static const char *TAG = "BUZZER";
 
 void BuzzerInit(int gpio_num, uint32_t freq, uint32_t duration)
 {
-	ledc_timer_config_t timer_conf;
-	timer_conf.speed_mode = LEDC_LOW_SPEED_MODE;	
-	timer_conf.timer_num = LEDC_TIMER_0;
-	timer_conf.duty_resolution = LEDC_TIMER_13_BIT;
-	timer_conf.freq_hz = freq;
-	timer_conf.clk_cfg = LEDC_AUTO_CLK;
-	ledc_timer_config(&timer_conf);
+	// ledc_timer_config_t timer_conf;
+	// timer_conf.speed_mode = LEDC_LOW_SPEED_MODE;	
+	// timer_conf.timer_num = LEDC_TIMER_0;
+	// timer_conf.duty_resolution = LEDC_TIMER_13_BIT;
+	// timer_conf.freq_hz = freq;
+	// timer_conf.clk_cfg = LEDC_AUTO_CLK;
+	// ledc_timer_config(&timer_conf);
 
-	ledc_channel_config_t ledc_conf;
-	ledc_conf.speed_mode = LEDC_LOW_SPEED_MODE;
-	ledc_conf.gpio_num = gpio_num;	
-	ledc_conf.channel = LEDC_CHANNEL_0;
-	ledc_conf.intr_type = LEDC_INTR_DISABLE;
-	ledc_conf.timer_sel = LEDC_TIMER_0;
-	ledc_conf.duty = 0x0; 
-	ledc_conf.hpoint = 0x00;						  
-	ledc_channel_config(&ledc_conf);
+	// ledc_channel_config_t ledc_conf;
+	// ledc_conf.speed_mode = LEDC_LOW_SPEED_MODE;
+	// ledc_conf.gpio_num = gpio_num;	
+	// ledc_conf.channel = LEDC_CHANNEL_0;
+	// ledc_conf.intr_type = LEDC_INTR_DISABLE;
+	// ledc_conf.timer_sel = LEDC_TIMER_0;
+	// ledc_conf.duty = 0x0; 
+	// ledc_conf.hpoint = 0x00;						  
+	// ledc_channel_config(&ledc_conf);
 
-	ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0);
-	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+	// ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0);
+	// ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
 }
 
 /**
@@ -50,12 +50,12 @@ void BuzzerInit(int gpio_num, uint32_t freq, uint32_t duration)
 static void BuzzerMakeSound(uint32_t duration)
 {	
 	// start
-	ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 819); // 10% duty - play here for your 
-	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-	vTaskDelay(duration / portTICK_PERIOD_MS);
-	// stop
-	ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0);
-	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+	// ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 819); // 10% duty - play here for your 
+	// ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+	// vTaskDelay(duration / portTICK_PERIOD_MS);
+	// // stop
+	// ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0);
+	// ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
 }
 
 /**
@@ -64,11 +64,11 @@ static void BuzzerMakeSound(uint32_t duration)
  */
 static void BuzzerBiz(uint8_t count)
 {
-	for (size_t i = 0; i < count; i++)
-	{
-		BuzzerMakeSound(CONFIG_DONE_BUZZER_DEFAULT_PWM_DURATION);
-		vTaskDelay(CONFIG_DONE_BUZZER_DEFAULT_DELAY_MS / portTICK_PERIOD_MS);
-	}
+	// for (size_t i = 0; i < count; i++)
+	// {
+	// 	BuzzerMakeSound(CONFIG_DONE_BUZZER_DEFAULT_PWM_DURATION);
+	// 	vTaskDelay(CONFIG_DONE_BUZZER_DEFAULT_DELAY_MS / portTICK_PERIOD_MS);
+	// }
 }
 
 /**
@@ -77,19 +77,19 @@ static void BuzzerBiz(uint8_t count)
  */
 void BuzzerPlay(BuzzerEffect effectNo)
 {
-	switch (effectNo)
-	{
-		case ONE_BIZ:
-			BuzzerBiz(ONE_BIZ);
-			break;
-		case DOUBLE_BIZ:
-			BuzzerBiz(DOUBLE_BIZ);
-			break;
-		case TRIPLE_BIZ:
-			BuzzerBiz(TRIPLE_BIZ);
-			break;
-		case NO_EFFECT:
-		default:
-			break;
-	}
+	// switch (effectNo)
+	// {
+	// 	case ONE_BIZ:
+	// 		BuzzerBiz(ONE_BIZ);
+	// 		break;
+	// 	case DOUBLE_BIZ:
+	// 		BuzzerBiz(DOUBLE_BIZ);
+	// 		break;
+	// 	case TRIPLE_BIZ:
+	// 		BuzzerBiz(TRIPLE_BIZ);
+	// 		break;
+	// 	case NO_EFFECT:
+	// 	default:
+	// 		break;
+	// }
 }
