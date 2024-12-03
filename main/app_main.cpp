0637567110917208f9e7b0b2ab07e2671f1560aa
ehsanziyaee <ehsan.ziyaee@gmail.com>
2024-12-03 13:49:23 +0330

    remove ram logs

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -34,27 +34,6 @@ extern "C" void app_main()
     heartBeatConf.pull_up_en = GPIO_PULLUP_DISABLE;
     gpio_config(&heartBeatConf);    
 
-    ESP_LOGW(
-        TAG,
-        "\n"
-        "  Free Heap:           %u  bytes\n"
-        "  MALLOC_CAP_8BIT      %7zu bytes\n"
-        "  MALLOC_CAP_DMA       %7zu bytes\n"
-        "  MALLOC_CAP_SPIRAM    %7zu bytes\n"
-        "  MALLOC_CAP_INTERNAL  %7zu bytes\n"
-        "  MALLOC_CAP_DEFAULT   %7zu bytes\n"
-        "  MALLOC_CAP_IRAM_8BIT %7zu bytes\n"
-        "  MALLOC_CAP_RETENTION %7zu bytes\n",
-        xPortGetFreeHeapSize(),
-        heap_caps_get_free_size(MALLOC_CAP_8BIT),
-        heap_caps_get_free_size(MALLOC_CAP_DMA),
-        heap_caps_get_free_size(MALLOC_CAP_SPIRAM),
-        heap_caps_get_free_size(MALLOC_CAP_INTERNAL),
-        heap_caps_get_free_size(MALLOC_CAP_DEFAULT),
-        heap_caps_get_free_size(MALLOC_CAP_IRAM_8BIT),
-        heap_caps_get_free_size(MALLOC_CAP_RETENTION)
-    );
-
     while (true)
     {
         for (int i = 0; i < HeartbeatPatternLength; i++) 
