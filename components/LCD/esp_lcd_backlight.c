54857ee9b391d85f22033b494f12387a7f72004d
MohammadAbbasi76 <work.abbasii@gmail.com>
2025-01-12 14:43:05 +0330

    update backlight sources

--- components/LCD/esp_lcd_backlight.c
+++ components/LCD/esp_lcd_backlight.c
@@ -13,42 +13,14 @@
 static const char *TAG = "LCD_backlight";
 
 #define BACKLIGHT_LEDC_FRQUENCY 5000
-
+#ifdef CONFIG_TFT_BACKLIGHT_ENABLE
 // Backlight configuration based on kconfig options
 static const ledc_mode_t BACKLIGHT_LEDC_MODE = LEDC_LOW_SPEED_MODE;
 
-#if CONFIG_BACKLIGHT_LEDC_CHANNEL_0
-static const ledc_channel_t BACKLIGHT_LEDC_CHANNEL = LEDC_CHANNEL_0;
-#elif CONFIG_BACKLIGHT_LEDC_CHANNEL_1
-static const ledc_channel_t BACKLIGHT_LEDC_CHANNEL = LEDC_CHANNEL_1;
-#elif CONFIG_BACKLIGHT_LEDC_CHANNEL_2
-static const ledc_channel_t BACKLIGHT_LEDC_CHANNEL = LEDC_CHANNEL_2;
-#elif CONFIG_BACKLIGHT_LEDC_CHANNEL_3
-static const ledc_channel_t BACKLIGHT_LEDC_CHANNEL = LEDC_CHANNEL_3;
-#elif CONFIG_BACKLIGHT_LEDC_CHANNEL_4
-static const ledc_channel_t BACKLIGHT_LEDC_CHANNEL = LEDC_CHANNEL_4;
-#elif CONFIG_BACKLIGHT_LEDC_CHANNEL_5
-static const ledc_channel_t BACKLIGHT_LEDC_CHANNEL = LEDC_CHANNEL_5;
-#else
-#error "No valid LEDC channel selected"
-#endif
 
-#if CONFIG_BACKLIGHT_LEDC_TIMER_0
-static const ledc_timer_t BACKLIGHT_LEDC_TIMER = LEDC_TIMER_0;
-#elif CONFIG_BACKLIGHT_LEDC_TIMER_1
-static const ledc_timer_t BACKLIGHT_LEDC_TIMER = LEDC_TIMER_1;
-#elif CONFIG_BACKLIGHT_LEDC_TIMER_2
-static const ledc_timer_t BACKLIGHT_LEDC_TIMER = LEDC_TIMER_2;
-#else
-#error "No valid LEDC timer selected"
-#endif
-
-#if CONFIG_BACKLIGHT_LEDC_TIMER_10_BIT
-static const ledc_timer_bit_t BACKLIGHT_LEDC_TIMER_RESOLUTION = LEDC_TIMER_10_BIT;
-#elif CONFIG_BACKLIGHT_LEDC_TIMER_12_BIT
-static const ledc_timer_bit_t BACKLIGHT_LEDC_TIMER_RESOLUTION = LEDC_TIMER_12_BIT;
-#else
-#error "No valid LEDC timer resolution selected"
+static const ledc_channel_t backLight_LEDC_Channel = BACKLIGHT_LEDC_CHANNEL;
+static const ledc_timer_t  backLight_LEDC_Timer = BACKLIGHT_LEDC_TIMER;
+static const ledc_timer_bit_t  backLight_LEDC_TimResolution = BACKLIGHT_LEDC_TIMER_RESOLUTION;
 #endif
 
 void display_brightness_init(void)
@@ -57,9 +29,9 @@ void display_brightness_init(void)
         {
             .gpio_num = (gpio_num_t)CONFIG_TFT_BACKLIGHT_PIN,
             .speed_mode = BACKLIGHT_LEDC_MODE,
-            .channel = BACKLIGHT_LEDC_CHANNEL,
+            .channel =  backLight_LEDC_Channel,
             .intr_type = LEDC_INTR_DISABLE,
-            .timer_sel = BACKLIGHT_LEDC_TIMER,
+            .timer_sel =  backLight_LEDC_Timer,
             .duty = 0,
             .hpoint = 0,
             .flags =
@@ -68,8 +40,8 @@ void display_brightness_init(void)
     const ledc_timer_config_t LCD_backlight_timer =
         {
             .speed_mode = BACKLIGHT_LEDC_MODE,
-            .duty_resolution = BACKLIGHT_LEDC_TIMER_RESOLUTION,
-            .timer_num = BACKLIGHT_LEDC_TIMER,
+            .duty_resolution =  backLight_LEDC_TimResolution,
+            .timer_num =  backLight_LEDC_Timer,
             .freq_hz = BACKLIGHT_LEDC_FRQUENCY,
             .clk_cfg = LEDC_AUTO_CLK};
     ESP_LOGI(TAG, "Initializing LEDC for backlight pin: %d", CONFIG_TFT_BACKLIGHT_PIN);
@@ -92,6 +64,6 @@ void display_brightness_set(int brightness_percentage)
 
     // LEDC resolution set to 10bits, thus: 100% = 1023
     uint32_t duty_cycle = (1023 * brightness_percentage) / 100;
-    ESP_ERROR_CHECK(ledc_set_duty(BACKLIGHT_LEDC_MODE, BACKLIGHT_LEDC_CHANNEL, duty_cycle));
-    ESP_ERROR_CHECK(ledc_update_duty(BACKLIGHT_LEDC_MODE, BACKLIGHT_LEDC_CHANNEL));
+    ESP_ERROR_CHECK(ledc_set_duty(BACKLIGHT_LEDC_MODE,  backLight_LEDC_Channel, duty_cycle));
+    ESP_ERROR_CHECK(ledc_update_duty(BACKLIGHT_LEDC_MODE,  backLight_LEDC_Channel));
 }
