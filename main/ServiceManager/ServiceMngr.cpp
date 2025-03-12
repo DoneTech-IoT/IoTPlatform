115d078c2a0b3920eff9a7226e173ef3842a6fb4
ehsanziyaee <ehsan.ziyaee@gmail.com>
2025-03-12 18:07:28 +0330

    start to implement ServiceMngr class

---
+++ main/ServiceManager/ServiceMngr.cpp
@@ -0,0 +1,158 @@
+#ifdef CONFIG_DONE_COMPONENT_LVGL
+//#include "GUIService.hpp"
+//#include "coffeeMaker_GUI.h"
+#endif
+#ifdef CONFIG_DONE_COMPONENT_MATTER
+#include "MatterService.hpp"
+#else
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#endif
+#ifdef CONFIG_DONE_LOG
+#include "Custom_Log.h"
+#endif
+#ifdef CONFIG_DONE_COMPONENT_MQTT
+//#include "MQTTSErvise.hpp"
+#endif
+
+#include "ServiceMngr.hpp"
+#include "SharedBus.hpp"
+#include "Singleton.hpp"
+#include "nvsFlash.h"
+#include <cstring>
+
+const char* ServiceMngr::pTAG = "ServiceMngr";
+ServiceMngr::ServiceParams_t ServiceMngr::mServiceParams[SharedBus::ServiceID::MAX_ID];
+
+#ifdef CONFIG_DONE_COMPONENT_LVGL
+    TaskHandle_t ServiceMngr::LVGLHandle = NULL;
+#endif  //CONFIG_DONE_COMPONENT_LVGL
+
+#ifdef CONFIG_DONE_COMPONENT_MATTER
+    TaskHandle_t ServiceMngr::MatterHandle = NULL;
+#endif  //CONFIG_DONE_COMPONENT_MATTER
+
+#ifdef CONFIG_DONE_COMPONENT_MQTT
+    TaskHandle_t ServiceMngr::MQTTHandle = NULL;
+#endif  //CONFIG_DONE_COMPONENT_MQTT
+
+ServiceMngr::ServiceMngr(
+    const char *TaskName,
+    const SharedBus::ServiceID &ServiceID) :
+    ServiceBase(TaskName, ServiceID)
+{
+
+}
+
+ServiceMngr::~ServiceMngr()
+{        
+}
+
+/**
+* @brief run given service.
+* This function runs the given service by initializing the service parameters and creating the task.
+* @param[in] serviceParams Service parameters    
+* @retval ESP_OK if the service is run successfully, otherwise ESP_FAIL
+*/ 
+esp_err_t ServiceMngr::RunService(ServiceParams_t serviceParams)
+{
+    esp_err_t err = ESP_OK;
+
+    // Call the function pointer (init_func) with proper arguments
+    TaskInitPtr init_func = serviceParams.taskInit;
+    err = init_func(&serviceParams.taskHandler, 
+                    serviceParams.priority, 
+                    serviceParams.taskStackSize);
+    if (err != ESP_OK) 
+    {
+        ESP_LOGE(pTAG, "Failed to create %s!", serviceParams.name);
+        return err;
+    }
+
+    return ESP_OK;    
+}
+
+/**
+* @brief Deletes a task.
+* This function deletes the specified task.
+* @param ServiceID The service ID to be deleted.
+* @return void
+*/
+void ServiceMngr::KillService(const SharedBus::ServiceID &ServiceID)
+{
+    
+    ESP_LOGI(pTAG, "%s service was Deleted !", mServiceParams[ServiceID].name);
+    mServiceParams[ServiceID].taskKiller(&mServiceParams[ServiceID].taskHandler);
+}
+
+/**
+ * @brief Starts all registered daemon services.
+ * This function iterates through all registered services and calls their `Start()` method.
+ * Each service is expected to be a subclass of `ServiceBase` and implement its own startup logic.
+ */
+void ServiceMngr::RunAllDaemons()
+{
+    esp_err_t err;
+
+// #ifdef CONFIG_DONE_COMPONENT_LVGL    
+//     ServiceParams_t GUIParams;    
+//     strcpy(GUIParams.name, "GUI");    
+//     GUIParams.TaskKiller = GUI;
+//     GUIParams.taskStack = LVGL_STACK;
+//     GUIParams.priority = tskIDLE_PRIORITY + 1;  
+//     GUIParams.taskHandler = LVGLHandle;  
+//     GUIParams.TaskInit = GUI_TaskInit;
+//     GUI_OnInitElements(GUI_Init);
+//     GUI_OnSharedBusReceived(GUI_ProcessSharedBusMsg);
+//     err = ServiceManager_RunService(GUIParams);
+//     if (err)
+//     {
+//         ESP_LOGE(TAG, "Failed to create GUI!");
+//     }
+//     else
+//     {
+//         ESP_LOGI(TAG, "GUI Daemon Created !");        
+//     }    
+// #endif //CONFIG_DONE_COMPONENT_LVGL
+
+#ifdef CONFIG_DONE_COMPONENT_MATTER    
+    ServiceParams_t MatterParams;
+    strcpy(MatterParams.name, "Matter");    
+    MatterParams.taskHandler = MatterHandle;
+    MatterParams.taskInit = MatterService::InitTask;
+    MatterParams.taskKiller =  MatterService::~MatterService;
+    MatterParams.taskStackSize = mServiceStackSize[SharedBus::ServiceID::MATTER];
+    MatterParams.priority = tskIDLE_PRIORITY + 1;
+        
+    err = RunService(MatterParams);
+    if (err)
+    {
+        ESP_LOGE(pTAG, "Failed to create Matter !");
+    }
+    else 
+    {
+        ESP_LOGI(pTAG, "Matter Daemon Created !");
+    }
+#endif //CONFIG_DONE_COMPONENT_MATTER
+    
+// #ifdef CONFIG_DONE_COMPONENT_MQTT    
+//     ServiceParams_t MQTTParams;
+//     strcpy(MQTTParams.name, "MQTT");
+//     MQTTParams.maximumRAM_Needed = 0;                
+//     MQTTParams.ramType = SRAM_;
+//     MQTTParams.TaskKiller = MQTT_TaskKill;
+//     MQTTParams.taskStack = MQTT_STACK;
+//     MQTTParams.priority = tskIDLE_PRIORITY + 1;
+//     MQTTParams.taskHandler = MQTTHandle;
+//     MQTTParams.TaskInit = MQTT_TaskInit;
+//     err = ServiceManager_RunService (MQTTParams);
+//     if (err)
+//     {
+//         ESP_LOGE(TAG, "Failed to create MQTT !");
+//     }    
+//     else 
+//     {
+//         ESP_LOGI(TAG, "MQTT Daemon Created !");
+//     }
+// #endif //CONFIG_DONE_COMPONENT_MQTT    
+}
--- main/ServiceManager/ServiceMngr.hpp
+++ main/ServiceManager/ServiceMngr.hpp
@@ -1,7 +1,5 @@
 #pragma once 
 #include <stdint.h>
-#include <stdio.h>
-#include "string.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -9,18 +7,10 @@
 #include "esp_freertos_hooks.h"
 #include "freertos/semphr.h"
 
-#include "driver/gpio.h"
-
 #include <esp_event.h>
 #include "esp_log.h"
 #include "esp_system.h"
 #include <sys/param.h>
-
-#define NUMBER_OF_COSTUME_TASK 10
-#define LVGL_STACK 100 * 1024 // in word not byte
-#define SERVICE_MANGER_STACK 20 * 1024
-#define MATTER_STACK_SIZE 50 * 1024
-#define MQTT_STACK 50 * 1024
 
 #include "ServiceBase.hpp"
 #include "SharedBus.hpp"
@@ -32,19 +22,32 @@ public:
         const char *TaskName,
         const SharedBus::ServiceID &ServiceID);
 
-    ~ServiceMngr() {}
+    ~ServiceMngr();
 
 private:
     static const char *pTAG;  
+
+#ifdef CONFIG_DONE_COMPONENT_LVGL
+    static TaskHandle_t LVGLHandle;
+#endif  //CONFIG_DONE_COMPONENT_LVGL
+
+#ifdef CONFIG_DONE_COMPONENT_MATTER
+    static TaskHandle_t MatterHandle;
+#endif  //CONFIG_DONE_COMPONENT_MATTER
+
+#ifdef CONFIG_DONE_COMPONENT_MQTT
+    static TaskHandle_t MQTTHandle;
+#endif  //CONFIG_DONE_COMPONENT_MQTT
+
     static constexpr uint32_t mServiceStackSize[SharedBus::ServiceID::MAX_ID] =
-    [
+    {
         0 ,             //NO_ID
         20  * 1024,     //SERVICE_MANAGER
         100 * 1024,     //UI
         50  * 1024,     //MATTER
         50  * 1024,     //MQTT
         0               //LOG
-    ];
+    };
 
     typedef void (*TaskCreatorPtr)(void);
     typedef void (*TaskKillerPtr)(TaskHandle_t *);
@@ -53,12 +56,39 @@ private:
                             UBaseType_t taskPriority,
                             uint32_t taskStackSize);
     typedef struct
-    {        
-        UBaseType_t priority;     
-        TaskHandle_t taskHandler; 
+    {           
+        char name[32];  //Service name    
+        uint8_t id;     //Service if from SharedBus::ServiceID
+        UBaseType_t priority;     //Priority of the task
+        TaskHandle_t taskHandler; //Pointer to the task handler function
         TaskCreatorPtr taskCreator;        
         TaskKillerPtr taskKiller;
         TaskInitPtr taskInit;        
         uint32_t taskStackSize;        
-    } ServiceParams;
+    } ServiceParams_t;
+
+    static ServiceParams_t mServiceParams[SharedBus::ServiceID::MAX_ID];    
+
+    /**
+    * @brief run given service.
+    * This function runs the given service by initializing the service parameters and creating the task.
+    * @param[in] serviceParams Service parameters    
+    * @retval ESP_OK if the service is run successfully, otherwise ESP_FAIL
+    */ 
+    esp_err_t RunService(ServiceParams_t serviceParams);
+
+    /**
+     * @brief Starts all registered daemon services.
+     * This function iterates through all registered services and calls their `Start()` method.
+     * Each service is expected to be a subclass of `ServiceBase` and implement its own startup logic.     
+     */
+    void RunAllDaemons();
+
+    /**
+    * @brief Deletes a task.
+    * This function deletes the specified task.
+    * @param ServiceID The service ID to be deleted.
+    * @return void
+    */
+    void KillService(const SharedBus::ServiceID &ServiceID);
 };
