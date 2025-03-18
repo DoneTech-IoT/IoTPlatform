590ffce4890366af92b6bd7fc162a02cf740718d
ehsanziyaee <ehsan.ziyaee@gmail.com>
2025-03-18 17:54:28 +0330

    change classic svrmng by modern one

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -4,6 +4,19 @@
 #include "Custom_Log.h"
 #include "driver/gpio.h"
 #include "esp_heap_caps.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+#include "ServiceMngr.hpp"
+
+void check_memory() {
+    ESP_LOGI("MEM", "Free heap memory:     %lu bytes", 
+        (uint32_t)esp_get_free_heap_size());
+    ESP_LOGI("MEM", "Free internal memory: %lu bytes", 
+        (uint32_t)heap_caps_get_free_size(MALLOC_CAP_INTERNAL));
+    ESP_LOGI("MEM", "Free PSRAM:           %lu bytes", 
+        (uint32_t)heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
+}
 
 #define HEARTBEAT_GPIO GPIO_NUM_21
 
@@ -22,9 +35,14 @@ static const char *TAG = "Main";
  */
 extern "C" void app_main()
 {
-    Log_RamOccupy("main", "service manager");
-    ServiceManger_TaskInit();
-    Log_RamOccupy("main", "service manager");
+    // Log_RamOccupy("main", "service manager");
+    // ServiceManger_TaskInit();
+    // Log_RamOccupy("main", "service manager");
+    check_memory();
+    ServiceMngr serviceMngr(
+        ServiceMngr::mServiceName[static_cast<uint8_t>(SharedBus::ServiceID::SERVICE_MANAGER)],
+        SharedBus::ServiceID::SERVICE_MANAGER);
+    check_memory();
 
     gpio_config_t heartBeatConf;
     heartBeatConf.intr_type = GPIO_INTR_DISABLE;
